845304a4ce3d0de839560218285bfe6fcb0c596b
Pia Anoop B <piab1703@gmail.com>
2026-09-08 23:51:02 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeElements(self, head, val):
+        while head and head.val == val :
+            head = head.next
+
+        curr = head
+
+        while curr and curr.next:
+            if curr.next.val == val :
+                curr.next = curr.next.next
+            
+            else :
+                curr = curr.next
+            
+        return head 
